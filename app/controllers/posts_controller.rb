638b79a5c9819d4db74a9098e49b992e3a7e14d5
Liz Daly <liz7daly@gmail.com>
2020-03-17 19:18:48 +0000

    delete unused tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,12 +63,12 @@ class PostsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:description, :picture)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:description, :picture)
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe PostsController, type: :controller do
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { description: 'picture description', picture: '/uploads/post/picture/4/picture.jpg'}
   }
 
   let(:invalid_attributes) {
@@ -39,103 +39,108 @@ RSpec.describe PostsController, type: :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # PostsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { authentication }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Post.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET #index" do
+  #   it "responds with 302 if not signed in" do
+  #     get :new
+  #     expect(response).to have_http_status(302)
+  #   end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      post = Post.create! valid_attributes
-      get :show, params: { id: post.to_param }, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  #   # it "returns a success response if signed in" do
+  #   #   Post.create! valid_attributes
+  #   #   get :index, params: {}, session: valid_session
+  #   #   expect(response).to be_successful
+  #   # end
+  # end
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET #show" do
+  #   it "returns a success response" do
+  #     post = Post.create! valid_attributes
+  #     get :show, params: { id: post.to_param }, session: valid_session
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      post = Post.create! valid_attributes
-      get :edit, params: { id: post.to_param }, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET #new" do
+  #   it "returns a success response" do
+  #     get :new, params: {}, session: valid_session
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Post" do
-        expect {
-          post :create, params: { post: valid_attributes }, session: valid_session
-        }.to change(Post, :count).by(1)
-      end
+  # describe "GET #edit" do
+  #   it "returns a success response" do
+  #     post = Post.create! valid_attributes
+  #     get :edit, params: { id: post.to_param }, session: valid_session
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-      it "redirects to the created post" do
-        post :create, params: { post: valid_attributes }, session: valid_session
-        expect(response).to redirect_to(Post.last)
-      end
-    end
+  # describe "POST #create" do
+  #   context "with valid params" do
+  #     it "creates a new Post" do
+  #       expect {
+  #         post :create, params: { post: valid_attributes }, session: valid_session
+  #       }.to change(Post, :count).by(1)
+  #     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: { post: invalid_attributes }, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
+  #     it "redirects to the created post" do
+  #       post :create, params: { post: valid_attributes }, session: valid_session
+  #       expect(response).to redirect_to(Post.last)
+  #     end
+  #   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  #   context "with invalid params" do
+  #     it "returns a success response (i.e. to display the 'new' template)" do
+  #       post :create, params: { post: invalid_attributes }, session: valid_session
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-      it "updates the requested post" do
-        post = Post.create! valid_attributes
-        put :update, params: { id: post.to_param, post: new_attributes }, session: valid_session
-        post.reload
-        skip("Add assertions for updated state")
-      end
+  # describe "PUT #update" do
+  #   context "with valid params" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-      it "redirects to the post" do
-        post = Post.create! valid_attributes
-        put :update, params: { id: post.to_param, post: valid_attributes }, session: valid_session
-        expect(response).to redirect_to(post)
-      end
-    end
+  #     it "updates the requested post" do
+  #       post = Post.create! valid_attributes
+  #       put :update, params: { id: post.to_param, post: new_attributes }, session: valid_session
+  #       post.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        post = Post.create! valid_attributes
-        put :update, params: { id: post.to_param, post: invalid_attributes }, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
+  #     it "redirects to the post" do
+  #       post = Post.create! valid_attributes
+  #       put :update, params: { id: post.to_param, post: valid_attributes }, session: valid_session
+  #       expect(response).to redirect_to(post)
+  #     end
+  #   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested post" do
-      post = Post.create! valid_attributes
-      expect {
-        delete :destroy, params: { id: post.to_param }, session: valid_session
-      }.to change(Post, :count).by(-1)
-    end
+  #   context "with invalid params" do
+  #     it "returns a success response (i.e. to display the 'edit' template)" do
+  #       post = Post.create! valid_attributes
+  #       put :update, params: { id: post.to_param, post: invalid_attributes }, session: valid_session
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-    it "redirects to the posts list" do
-      post = Post.create! valid_attributes
-      delete :destroy, params: { id: post.to_param }, session: valid_session
-      expect(response).to redirect_to(posts_url)
-    end
-  end
+  # describe "DELETE #destroy" do
+  #   it "destroys the requested post" do
+  #     post = Post.create! valid_attributes
+  #     expect {
+  #       delete :destroy, params: { id: post.to_param }, session: valid_session
+  #     }.to change(Post, :count).by(-1)
+  #   end
+
+  #   it "redirects to the posts list" do
+  #     post = Post.create! valid_attributes
+  #     delete :destroy, params: { id: post.to_param }, session: valid_session
+  #     expect(response).to redirect_to(posts_url)
+  #   end
+  # end
 
 end
